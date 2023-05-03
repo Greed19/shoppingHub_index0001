9cd780563f45df2cc31bb7688cb46c4ca2367b5f
Greed19 <aldriep.antonio@gmail.com>
2023-05-04 00:08:05 +0800

    responsivenessv1

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { STATUS } from '../../utils/status';
 import Error from '../Error/Error';
 import Loader from '../Loader/Loader';
@@ -8,9 +9,14 @@ import styles from './Category.module.scss';
 
 const Category = ({categories,status}) => {
   const newCat = categories.slice(0,5)
+  const navigate = useNavigate();
   if(status === STATUS.ERROR) return (<Error/>)
   if(status === STATUS.LOADING) return (<Loader/>)
-  
+  const handleClick = (item) => {
+    const {id,name} = item
+    navigate(`category/${id}`)
+    localStorage.setItem('catName', name)
+  }
   return (
     <section className={styles.category_main_container}>
       <h3 className={styles.title}>Category</h3>
@@ -18,7 +24,7 @@ const Category = ({categories,status}) => {
         <div className={styles.categories_map}>
           {
             newCat.map((item,cNdx) => (
-              <div key={`${item.id} ${cNdx} `} className={styles.category_container}>
+              <div key={`${item.id} ${cNdx} `} className={styles.category_container} onClick={() => handleClick(item)}>
                 <img className={styles.cat_img} src={item.image} alt="" />
                 <span className={styles.category_name}>{item.name}</span>
               </div>
